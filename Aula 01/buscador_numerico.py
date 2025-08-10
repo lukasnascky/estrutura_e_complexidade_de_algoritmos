c35828a9c0a9310e3dcc5631057a8684e7ad54d9
Lucas Nascimento <159586908+lukasnascky@users.noreply.github.com>
2025-08-10 15:42:16 -0300

    Update 10.08.2025

--- Aula 01/buscador_numerico.py
+++ Aula 01/buscador_numerico.py
@@ -1,6 +1,17 @@
+import time
+import psutil
+import os
+
 arquivo = "Aula 01/numeros.txt"
 
-valor_a_econtrar = 34
+numero_procurado = int(input("Digite o número que deseja buscar: "))
+
+
+processo = psutil.Process(os.getpid())
+
+inicio_tempo = time.perf_counter()
+cpu_inicial = processo.cpu_times()
+memoria_inicial = processo.memory_info().rss
 
 with open(arquivo, 'r') as c:
     conteudo = c.read()
@@ -9,6 +20,26 @@ with open(arquivo, 'r') as c:
 encontrado = False
 
 for numero in lista_numeros:
-    if numero == valor_a_econtrar:
+    if numero == numero_procurado:
         encontrado = True
         break
+
+fim_tempo = time.perf_counter()
+cpu_final = processo.cpu_times()
+memoria_final = processo.memory_info().rss
+
+
+tempo_execucao = fim_tempo - inicio_tempo
+uso_cpu = (cpu_final.user - cpu_inicial.user) + (cpu_final.system - cpu_inicial.system)
+uso_memoria = memoria_final - memoria_inicial
+
+# Resultados
+if encontrado:
+    print(f"O número {numero_procurado} foi encontrado!")
+else:
+    print(f"O número {numero_procurado} não foi encontrado.")
+
+print(f"\nEstatísticas de execução:")
+print(f"Tempo de execução: {tempo_execucao:.6f} segundos")
+print(f"Uso de CPU: {uso_cpu:.10f} segundos de CPU")
+print(f"Consumo de memória: {uso_memoria / 1024:.2f} KB")
